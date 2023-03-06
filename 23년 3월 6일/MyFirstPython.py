644e4b8ee93c927ad10df09b46d4d5cd089db47c
Hyun0907 <118407596+Hyun05@users.noreply.github.com>
2023-03-06 15:07:08 +0900

    Add files via upload

---
+++ 23년 3월 6일/MyFirstPython.py
@@ -0,0 +1,17 @@
+print("안산공업고등학교");
+print("컴퓨터과 3학년 1반 2번");
+print("강현묵")
+print("300" + "400");
+print(300 + 400);
+
+
+temp1 = "안녕하세요"
+temp2 = "강현묵 입니다"
+
+print(temp1 + temp2);
+
+school = "안녕하세요"
+dept = "컴퓨터과"
+name = "강현묵 입니다"
+
+print(school , dept , name)
